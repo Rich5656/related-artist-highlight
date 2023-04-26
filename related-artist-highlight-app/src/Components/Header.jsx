8784679561efdccd9b12dd837e5c3855a9a3945a
Rich5656 <richard.k.lowther@gmail.com>
2023-04-26 11:40:10 -0400

    Added responsive breakpoints to nav-bar and change the intro paragraph formatting

--- related-artist-highlight-app/src/Components/Header.jsx
+++ related-artist-highlight-app/src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { ArtistInput } from './ArtistInput';
 export const Header = ({ artistSearch, setArtistSearch, fetchSearch, renderDropdown, searchResults }) => {
   return (
     <>
-      <div className='navbar bg-dark sticky-top'>
+      <div className='navbar bg-dark sticky-top justify-content-md-between justify-content-center'>
         <h2 className='text-center p-3'>Related Aritsts Highlight</h2>
         < ArtistInput artistSearch={artistSearch} setArtistSearch={setArtistSearch}
         fetchSearch={fetchSearch} renderDropdown={renderDropdown} searchResults={searchResults}
