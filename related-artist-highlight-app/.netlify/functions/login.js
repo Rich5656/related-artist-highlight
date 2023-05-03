7bdabfa63162fc5c785f780f4250484959b2db02
Rich5656 <richard.k.lowther@gmail.com>
2023-05-02 21:03:07 -0400

    Further updates to netlify functions

--- related-artist-highlight-app/.netlify/functions/login.js
+++ related-artist-highlight-app/.netlify/functions/login.js
@@ -1,42 +1,60 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
-exports.handler = async (event) => {
-    console.log(event);
-    // const code = req.body.code;
-    const code = event.queryStringParameters.code;
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000', // change to netlify adress for set up
-        clientId: process.env.SPOTIFY_ID, 
-        clientSecret: process.env.SPOTIFY_SECRET, 
-    });
 
+const handler = async (event) => {
+    console.log(event.queryStringParameters.code);
+    // const code = event.queryStringParameters.code;
+    // const spotifyApi = new SpotifyWebApi({
+    //     redirectUri: 'http://localhost:8888', // change to netlify adress for set up
+    //     clientId: process.env.SPOTIFY_ID, 
+    //     clientSecret: process.env.SPOTIFY_SECRET, 
+    // });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ someText: 'hello'})
+    };
+    
     // try {
-    //     const { data } = await spotifyApi.authorizationCodeGrant(code)
-
+    //     const data = spotifyApi.authorizationCodeGrant(code);
+    //     console.log(data);
     //     return {
     //         statusCode: 200,
-    //         body: JSON.stringify(data)
+    //         body: JSON.stringify({
+    //             accessToken: data.body.access_token,
+    //             refreshToken: data.body.refresh_token,
+    //             expiresIn: data.body.expires_in
+    //         })
+    //     }
+    // } catch (err) {
+    //     const { status, statusText, headers, data } = err.response;
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify(status, statusText, headers, data)
     //     }
     // }
-    
-    return spotifyApi.authorizationCodeGrant(code)
-        .then(data => {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    accessToken: data.body.access_token,
-                    refreshToken: data.body.refresh_token,
-                    expiresIn: data.body.expires_in
-                })
-            }
-        })
-        .catch((err) => {
-            const { status, statusText, headers, data } = err.response;
-            return {
-                statusCode: 400,
-                body: JSON.stringify(status, statusText, headers, data)
 
-            }
-        })
+
+    // spotifyApi.authorizationCodeGrant(code)
+    //     .then(data => {
+    //         return {
+    //             statusCode: 200,
+    //             body: JSON.stringify({
+    //                 accessToken: data.body.access_token,
+    //                 refreshToken: data.body.refresh_token,
+    //                 expiresIn: data.body.expires_in
+    //             })
+    //         }
+    //     })
+    //     .catch((err) => {
+    //         console.log(err)
+    //         // const { status, statusText, headers, data } = err.response;
+    //         // return {
+    //         //     statusCode: 400,
+    //         //     body: JSON.stringify(status, statusText, headers, data)
+    //         // }
+    //     })
 
 }
+
+module.exports = { handler }
